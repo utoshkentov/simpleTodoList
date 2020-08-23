5c101891996331499bc72138bc0655b22add74fa
Umrzoq Toshkentov <utoshkentov@gmail.com>
2020-08-23 19:33:25 +0500

    Updates

---
+++ precache-manifest.52095c675c8ad8b05141220290155f95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dce81cbe0dba1fe8b5eb539b69a79224",
+    "url": "/simpleTodoList/index.html"
+  },
+  {
+    "revision": "709dab6705d84927d28a",
+    "url": "/simpleTodoList/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "709dab6705d84927d28a",
+    "url": "/simpleTodoList/static/js/2.55093aa2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simpleTodoList/static/js/2.55093aa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0220d3e83f781c6b2a4d",
+    "url": "/simpleTodoList/static/js/main.ac49fce7.chunk.js"
+  },
+  {
+    "revision": "0c3b87112510474d9bc3",
+    "url": "/simpleTodoList/static/js/runtime-main.5e7769ae.js"
+  }
+]);
